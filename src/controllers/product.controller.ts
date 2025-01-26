359053b6e6d5e205f63d411e6e3d7ba99f9bf001
M Rifki Septiawan <rifkim91@gmail.com>
2025-01-26 09:12:41 +0700

    feat: createProduct controller function

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,9 @@
 import { Context } from "hono";
 import prisma from "../db/prisma";
+import { CreateProductInput, createProductSchema } from "../schemas/product.schema";
+import productRoutes from "../routes/product.routes";
 
-export const getProductByCode = async (c) => {
+export const getProductByCode = async (c: Context) => {
     const code = c.req.param();
     const product = await prisma.product.findUnique(
         {
@@ -19,3 +21,20 @@ export const getProductByCode = async (c) => {
 
     return c.json(product);
 }
+
+export const createProduct = async (c: Context) => {
+    const body: CreateProductInput = await c.req.json();
+    const validation = createProductSchema.safeParse(body);
+
+    if (!validation.success) {
+        return c.json({
+            message: 'invalid input',
+            errors: validation.error,
+        }, 400);
+    }
+
+    const product = await prisma.product.create({
+        data: body
+    });
+    return c.json(product, 201);
+};
