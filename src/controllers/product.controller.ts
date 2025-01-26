6f1e1835cfda0648c361eaff3d3de5c2241c9ca5
M Rifki Septiawan <rifkim91@gmail.com>
2025-01-26 08:36:05 +0700

    feat: product contoller added

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,21 @@
+import { Context } from "hono";
+import prisma from "../db/prisma";
+
+export const getProductByCode = async (c: Context) => {
+    const code = c.req.param();
+    const product = await prisma.product.findUnique(
+        {
+            where: {code}
+        }
+    );
+
+    if (!product) {
+        return c.json(
+            {
+                message: 'product not found'
+            }, 404,
+        )
+    }
+
+    return c.json(product);
+}
---
+++ src/types/product.ts
@@ -0,0 +1,6 @@
+export type Product = {
+    id: string;
+    code: string;
+    name: string;
+    price: number;
+  };
