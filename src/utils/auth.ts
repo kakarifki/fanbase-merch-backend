d158426d90ea34b92f015df80e302535fb177243
M Rifki Septiawan <rifkim91@gmail.com>
2025-02-06 19:23:26 +0700

    feat: backend installation betterAuth complete, trying on front end

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,24 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
- 
-const prisma = new PrismaClient();
+
+let prisma: PrismaClient;
+
+try {
+  prisma = new PrismaClient();
+  await prisma.$connect(); // Coba koneksi langsung
+  console.log("Koneksi ke database berhasil!");
+} catch (error) {
+  console.error("Gagal terhubung ke database:", error);
+  process.exit(1); // Hentikan aplikasi jika koneksi gagal
+}
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    emailAndPassword: {
+        enabled: true,
+    },
 });
+
